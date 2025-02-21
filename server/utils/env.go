baa805869208b423c3eacc0789efbedcdd065e77
CZJ <followmytype@gmail.com>
2025-02-21 10:58:23 +0800

    feat(server): 服務端啟動時可指定port

--- server/utils/env.go
+++ server/utils/env.go
@@ -1,24 +1,39 @@
 package utils
 
-import "os"
+import (
+	"os"
+	"strconv"
+)
 
 var serverAddr string
+var port string
 
 func init() {
-	if len(os.Args) > 1 {
-		serverAddr = os.Args[1]
-        return 
+	if len(os.Args) > 1 && isNumeric(os.Args[1]) {
+		port = os.Args[1]
+		serverAddr = "0.0.0.0:" + port
+		return
 	}
-    serverAddr = GetEnv("SERVER_ADDR", "localhost:8889")
+	port = GetEnv("PORT", "8889")
+	serverAddr = "0.0.0.0:" + port
 }
 
 func GetEnv(key, defaultValue string) string {
 	if value, exists := os.LookupEnv(key); exists {
-        return value
-    }
-    return defaultValue
+		return value
+	}
+	return defaultValue
+}
+
+func GetPort() string {
+	return port
 }
 
 func GetServerAddr() string {
-    return serverAddr
+	return serverAddr
+}
+
+func isNumeric(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
 }
